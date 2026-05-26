c59b61226f4999f7b2477eaaf8447eee5a47a69c
Smári Guðnason <smari@explorer.is>
2026-05-26 21:57:29 +0000

    fix backend

--- backend.js
+++ backend.js
@@ -27,6 +27,8 @@ app.get('/dk/test', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Backend running');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Backend running on port', PORT);
 });
